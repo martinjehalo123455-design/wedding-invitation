e94e1a5d70aad6ea16007a01c49c569acd9c4c80
Martin Jimenez Esmerio <martinjehalo.123455@gmail.com>
2026-09-11 11:07:54 -0600

    Se hicieron ajustes al formulario

--- js/form.js
+++ js/form.js
@@ -1,8 +1,18 @@
+const confirmation_label1 = document.getElementById("confirmation-label-1");
+const confirmation_label2 = document.getElementById("confirmation-label-2");
 const formulario = document.getElementById("confirmation-form");
 const botonEnviar = document.getElementById("form-button");
 
+
+function confirmed() {
+    formulario.remove();
+    confirmation_label2.remove();
+    confirmation_label1.innerHTML = "";
+    confirmation_label1.innerText = "!Gracias por confirmar tu asistencia!"
+}
+
 // Se le agrega un evento al formulario
-formulario.addEventListener("submit", function(e){
+formulario.addEventListener("submit", function(e) {
   e.preventDefault();
 
   const data = {
@@ -12,8 +22,6 @@ formulario.addEventListener("submit", function(e){
     mensaje:this.mensaje.value,
   };
 
-  botonEnviar.disabled = true;
-
   fetch("https://script.google.com/macros/s/AKfycbyd8-c9hDouIc48gHoB-LIdRLAVbe2OHj-ThNL205xC4u1MNKhCKZn2rZcdoEjubw/exec", {
     method: "POST",
     body: JSON.stringify(data)
@@ -21,14 +29,12 @@ formulario.addEventListener("submit", function(e){
   .then(res => {
     if (!res.ok) {
       throw new Error("Respuesta no exitosa del servidor");
+    } else {
+        alert("Respuesta enviada");
+        confirmed();
     }
-    alert("Se ha enviado tu respuesta, ¡Gracias por confirmar!");
-    formulario.reset();
   })
   .catch(error => {
     alert("Error al enviar");
   })
-  .finally(() => {
-    botonEnviar.disabled = false;
-  });
 });
